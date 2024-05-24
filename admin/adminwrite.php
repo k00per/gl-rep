feec9b8cf24bb2629b2431dfe297534129cd9adb
k00per <104530938+k00per@users.noreply.github.com>
2024-05-25 01:18:15 +0300

    Create adminwrite.php

---
+++ admin/adminwrite.php
@@ -0,0 +1,31 @@
+
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+<table>
+<tr>
+<td>
+ <?php
+ include "viewdir.php";
+ ?>
+
+
+ </td>
+
+ <td>
+
+<form action="fwrite.php" method="post">
+<center>
+Имя файла:<input type="text" name="temp1" size=15>
+<br>
+<textarea cols=100 rows=30 name="neww1" wrap="physical"></textarea></td></tr>
+<br>
+<input type="submit" name="submit" value="отправить"></td>
+&nbsp
+<input type="Reset" value="очистить форму">
+</center>
+</form>
+
+</td>
+</tr>
+</table>
+
+
